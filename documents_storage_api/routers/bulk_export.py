2e9fed630d06b136b98e565d8195fb4c12cd19c0
Tafeen <taafeenn@gmail.com>
2021-12-04 21:45:22 +0100

    Added removing export files after 24 hours, updated input types for bulk_export routes

--- documents_storage_api/routers/bulk_export.py
+++ documents_storage_api/routers/bulk_export.py
@@ -1,20 +1,20 @@
 import io
-from json import loads
-from datetime import datetime
+import threading
 import os
 import pathlib
-from typing import List
 import uuid
+import tarfile
+from json import dumps, loads
+from datetime import datetime, timedelta
+from typing import List, Optional
 from fastapi import APIRouter, Depends, Response
 from fastapi.params import Query
 from starlette.responses import FileResponse
 from models.account.base import AccountModel
-from models.common import  PydanticObjectId, UUIDFromString, flat_map
-from middlewares.require_auth import UserChecker, UserCheckerModel
+from models.common import  PydanticObjectId, PydanticUUIDString, UUIDFromString, flat_map
+from middlewares.require_auth import PermissionsChecker, UserChecker, UserCheckerModel
 from models.document.base import DocumentModel
-import tarfile
 from routers.documents import StringFromUUID
-import threading
 
 MEDIA_FILES_PATH = os.getcwd() + "/data/media_files/"
 TAR_FILE_PATHS = os.getcwd() + "/data/temp/"
@@ -26,7 +26,21 @@ router = APIRouter(
     dependencies=[Depends(UserChecker)],
 )
 
-def build_archive(account_id_to_notify: PydanticObjectId, media_files_ids: list):
+def remove_archive_file(file_path):
+    print("Trying to remove tempororay file:", file_path)
+    try:
+        os.remove(file_path)
+        print('Removed successfully')
+    except OSError as error:
+        print(error)
+        print("File path cannot be removed")
+
+def build_archive(
+    account_id_to_notify: PydanticObjectId,
+    accounts: Optional[bool]=False,
+    documents: Optional[dict]=None,
+    media_files_ids: Optional[list]=None
+):
     try:
         pathlib.Path(TAR_FILE_PATHS).mkdir(parents=True, exist_ok=True)
     except Exception as e:
@@ -35,12 +49,21 @@ def build_archive(account_id_to_notify: PydanticObjectId, media_files_ids: list)
     file_id = str(uuid.uuid4())
     file_path = TAR_FILE_PATHS + file_id + ".tar.gz"
     with tarfile.open(file_path, "w:gz") as tar:
-        # JSON file
-        json_string = str(DocumentModel.objects().to_json()).encode('utf-8')
-        json_bytes = io.BytesIO(json_string)
-        info = tarfile.TarInfo(name="file.json")
-        info.size = len(json_string)
-        tar.addfile(tarinfo=info, fileobj=json_bytes)
+        # Accounts file
+        if accounts:
+            json_string = str(AccountModel.objects().to_json()).encode('utf-8')
+            json_bytes = io.BytesIO(json_string)
+            info = tarfile.TarInfo(name="accounts.json")
+            info.size = len(json_string)
+            tar.addfile(tarinfo=info, fileobj=json_bytes)
+
+        # Documents file
+        if documents:
+            json_string = dumps(documents).encode('utf-8')
+            json_bytes = io.BytesIO(json_string)
+            info = tarfile.TarInfo(name="documents.json")
+            info.size = len(json_string)
+            tar.addfile(tarinfo=info, fileobj=json_bytes)
 
         # Media files
         if media_files_ids:
@@ -50,25 +73,47 @@ def build_archive(account_id_to_notify: PydanticObjectId, media_files_ids: list)
                         tar.add(entry.path, arcname=str(pathlib.Path(entry.name)))
 
     file_url = f'http://{os.getenv("API_HOST")}:{os.getenv("API_PORT")}/export/{file_id}'
-    notification = f'Archived files are available to be downloaded from: {file_url}'
+    notification = f'Archived files are available (until {datetime.now()+timedelta(hours=24)}) to be downloaded from: {file_url}'
     AccountModel.objects(id=account_id_to_notify).update_one(add_to_set__notifications=notification)
+    t1 = threading.Timer(60*60*24, remove_archive_file, [file_path])
+    t1.start()
 
-@router.post("",
+
+@router.post("/accounts",
              responses={
                  200: {"description": "Started creating archive"},
              }
              )
-async def start_bulk_export(
+async def start_accounts_export(
+    user: UserCheckerModel = Depends(UserChecker)
+):
+    '''
+    Export all accounts.
+    '''
+    if PermissionsChecker("admin", user['rank']):
+        build_archive(user['client_id'])
+        t1 = threading.Thread(target=build_archive, kwargs={'account_id_to_notify': user['client_id'], 'accounts': True})
+        t1.start()
+        return Response(status_code=200)
+    else:
+        return Response(status_code=403)
+
+@router.post("/documents",
+             responses={
+                 200: {"description": "Started creating archive"},
+             }
+             )
+async def start_documents_export(
     include_media: bool = True,
-    ids: List[str] = Query(None, description="If null, then all documents will be included in export"),
+    document_ids: List[PydanticObjectId] = Query(None, description="If null, then all documents will be included in export"),
     user: UserCheckerModel = Depends(UserChecker)
 ):
     '''
     Export documents with optional include of media files to compressed tar.
     '''
     # Get documents
-    if(ids is not None):
-        documents_from_db = loads(DocumentModel.objects(id__in=ids).to_json())
+    if(document_ids is not None):
+        documents_from_db = loads(DocumentModel.objects(id__in=document_ids).to_json())
     else:
         documents_from_db = loads(DocumentModel.objects.to_json())
     
@@ -78,18 +123,18 @@ async def start_bulk_export(
             media_files_ids.append(StringFromUUID(doc['media_files']))
         media_files_ids = flat_map(list(map(lambda x: list(map(lambda y: str(y), UUIDFromString(x))), media_files_ids)))
     
-    t1 = threading.Thread(target=build_archive, kwargs={'account_id_to_notify': user['client_id'], 'media_files_ids': media_files_ids})
+    t1 = threading.Thread(target=build_archive, kwargs={'account_id_to_notify': user['client_id'], 'documents': documents_from_db, 'media_files_ids': media_files_ids})
     t1.start()
 
     return Response(status_code=200)
 
 @router.post("/{file_id}",
              responses={
-                 200: {"description": "Downloaded temporary file successfully"},
+                 200: {"description": "Downloaded archive successfully"},
              }
              )
-async def download_bulk_export(
-    file_id: str = Query(None, description="Id of archive file"),
+async def download_export(
+    file_id: PydanticUUIDString = Query(None, description="Id of archive file"),
 ):
     '''
     Download exported documents (tar.gz file)
